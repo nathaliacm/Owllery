22ca784e3e095f9aaddbed8056f8afb54eae4966
Nathália Cardoso <crds.nathalia@gmail.com>
2020-08-14 12:11:21 -0300

    configurando swiftlint

--- Owllery/Owllery/SceneDelegate.swift
+++ Owllery/Owllery/SceneDelegate.swift
@@ -14,7 +14,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-      
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = ViewController() // Your initial view controller.
         window.makeKeyAndVisible()
@@ -22,4 +21,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
 }
-
