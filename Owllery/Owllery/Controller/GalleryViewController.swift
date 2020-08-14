6e010f4ee8596bef197e2b1390a1720c56ed2fe4
Nathália Cardoso <crds.nathalia@gmail.com>
2020-08-14 15:31:40 -0300

    configurando a tab bar e criando o modelo de arquitetura

---
+++ Owllery/Owllery/Controller/GalleryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListaAlbunsViewController.swift
+//  Owllery
+//
+//  Created by Nathalia Cardoso on 14/08/20.
+//  Copyright © 2020 Nathalia Cardoso. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Owllery"
+    }
+
+}
