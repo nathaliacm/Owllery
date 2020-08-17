4c4589879dce2a722d3163a5747e9b696ba744e9
Nathália Cardoso <crds.nathalia@gmail.com>
2020-08-17 10:09:03 -0300

    Inserindo image view e button e configurando image picker

--- Owllery/Owllery/Controller/NewAlbumViewController.swift
+++ Owllery/Owllery/Controller/NewAlbumViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class NewAlbumViewController: UIViewController {
-
+class NewAlbumViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    let albumImage: UIImageView = UIImageView()
+    let selectImageButton: UIButton = UIButton()
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +22,46 @@ class NewAlbumViewController: UIViewController {
         navigationItem.title = "Novo Álbum"
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Salvar", style: .plain, target: self, action: nil)
+        
+        self.setUpAlbumImage()
+        self.setUpSelectImageButton()
+        
+        self.imagePicker.delegate = self
+    }
+    
+    func setUpAlbumImage() {
+        albumImage.contentMode = UIView.ContentMode.scaleAspectFill
+        albumImage.frame.size.width = 100
+        albumImage.frame.size.height = 100
+        albumImage.layer.cornerRadius = 100/2
+        albumImage.clipsToBounds = true
+        albumImage.center = CGPoint(x: self.view.center.x, y: 220)
+        albumImage.backgroundColor = .systemGray
+        self.view.addSubview(albumImage)
+    }
+    
+    func setUpSelectImageButton() {
+        selectImageButton.frame.size.width = 100
+        selectImageButton.frame.size.height = 100
+        selectImageButton.layer.cornerRadius = 100/2
+        selectImageButton.center = CGPoint(x: self.view.center.x, y: 220)
+        self.view.addSubview(selectImageButton)
+        selectImageButton.setTitle("EDITAR", for: .normal)
+        selectImageButton.addTarget(self, action: #selector(self.buttonClicked(_:)), for: .touchUpInside)
+    }
+    
+    @objc func buttonClicked(_ sender: UIButton) {
+        self.imagePicker.allowsEditing = false
+        self.imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            albumImage.contentMode = UIView.ContentMode.scaleAspectFill
+            albumImage.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
